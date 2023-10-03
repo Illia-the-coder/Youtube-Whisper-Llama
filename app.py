f3ede60bb0e29b16efb825fde51b0052f21d0f42
Illia Liudogovskyi <Illia56@users.noreply.huggingface.co>
2023-10-03 15:29:23 +0000

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ from gradio_client import Client
 
 
 system_message = "\nYou are a helpful, respectful and honest assistant. Always answer as helpfully as possible, while being safe.  Your answers should not include any harmful, unethical, racist, sexist, toxic, dangerous, or illegal content. Please ensure that your responses are socially unbiased and positive in nature.\n\nIf a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don't know the answer to a question, please don't share false information."
+temperature = 0.9
+top_p = 0.6
+repetition_penalty = 1.2
 
 title = "Llama2 70B Chatbot"
 description = """
