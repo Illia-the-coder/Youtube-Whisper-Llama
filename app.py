79ac78be2e28daf95713e19e3cef70bbe57059ac
Illia Liudogovskyi <Illia56@users.noreply.huggingface.co>
2023-10-04 14:55:57 +0000

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ examples=[
 
 
 # Stream text
-def predict(message, chatbot, system_prompt="", temperature=0.9, max_new_tokens=4096):
+def predict(message, chatbot, system_prompt="", temperature=0.9, max_new_tokens=4096, top_p=0.6, repetition_penalty=1.0,):
     
     client = Client("https://ysharma-explore-llamav2-with-tgi.hf.space/")
     return client.predict(
@@ -35,13 +35,51 @@ def predict(message, chatbot, system_prompt="", temperature=0.9, max_new_tokens=
 			api_name="/chat_1"
     )
         
-
+additional_inputs=[
+    gr.Textbox("", label="Optional system prompt"),
+    gr.Slider(
+        label="Temperature",
+        value=0.9,
+        minimum=0.0,
+        maximum=1.0,
+        step=0.05,
+        interactive=True,
+        info="Higher values produce more diverse outputs",
+    ),
+    gr.Slider(
+        label="Max new tokens",
+        value=256,
+        minimum=0,
+        maximum=4096,
+        step=64,
+        interactive=True,
+        info="The maximum numbers of new tokens",
+    ),
+    gr.Slider(
+        label="Top-p (nucleus sampling)",
+        value=0.6,
+        minimum=0.0,
+        maximum=1,
+        step=0.05,
+        interactive=True,
+        info="Higher values sample more low-probability tokens",
+    ),
+    gr.Slider(
+        label="Repetition penalty",
+        value=1.2,
+        minimum=1.0,
+        maximum=2.0,
+        step=0.05,
+        interactive=True,
+        info="Penalize repeated tokens",
+    )
+]
 
 
 
 # Gradio Demo 
 with gr.Blocks(theme=gr.themes.Base()) as demo:
     gr.DuplicateButton()
-    gr.ChatInterface(predict, title=title, description=description, css=css, examples=examples) 
+    gr.ChatInterface(predict, title=title,additional_inputs=additional_inputs, description=description, css=css, examples=examples) 
         
 demo.queue().launch(debug=True)
